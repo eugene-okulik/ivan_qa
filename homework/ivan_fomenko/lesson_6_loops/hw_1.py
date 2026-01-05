78e1c1aaf33ee01f8a97b5ef0497fed2d2d33a3d
Ivan Fomenko <qa.ivan.rs@gmail.com>
2026-01-05 16:17:05 +0300

    homework 6

---
+++ homework/ivan_fomenko/lesson_6_loops/hw_1.py
@@ -0,0 +1,25 @@
+# программа для добавления "ing" в конец каждого слова
+
+# Условия для работы программы:
+# знаки препинания не пишутся внутри слов
+# если идет запятая или точка после слова, то знак идет после "ing"
+
+
+my_string = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+
+# вариант с использованием печати внутри цикла
+for word in my_string.split():
+    if word[-1] in ',.':
+        print(word[:-1] + 'ing' + word[-1], end=' ')
+    else:
+        print(word + 'ing', end=' ')
+
+# вариант с созданием нового списка
+result_str = []
+for word in my_string.split():
+    if word[-1] in ',.':
+        result_str.append(word[:-1] + 'ing' + word[-1])
+    else:
+        result_str.append(word + 'ing')
+
+print(f'"\n\n\n"второй вариант: {" ".join(result_str)}')
