465075eb631b48da225db80605f21487002e7dc4
Ivan Fomenko <ifomenko@voctiv.com>
2026-01-24 11:30:37 +0300

    made hw 11 fix lint x2

--- homework/ivan_fomenko/lesson_11_classes/hw_classes.py
+++ homework/ivan_fomenko/lesson_11_classes/hw_classes.py
@@ -3,57 +3,58 @@ class Book:
     exist_text = True
 
     def __init__(
-            self, 
-            book_name, 
-            author, 
-            pages, 
-            ISBN, 
-            reserved):
+            self,
+            book_name,
+            author,
+            pages,
+            ISBN,
+            reserved
+            ):
         self.book_name = book_name
         self.author = author
         self.pages = pages
         self.ISBN = ISBN
         self.reserved = reserved
- 
+
 
 # пишу список книг
 Dostoevskyi_idiot = Book(
-    'Idiot', 
-    'Dostoevskyi', 
-    350, 
-    '978-5-94708-243-2', 
+    'Idiot',
+    'Dostoevskyi',
+    350,
+    '978-5-94708-243-2',
     True
     )
 
 Tolkien_lord_of_the_ring = Book(
-    'LOR', 
-    'Tolkien', 
-    1500, 
-    '978-5-17-106077-0', 
+    'LOR',
+    'Tolkien',
+    1500,
+    '978-5-17-106077-0',
     False
     )
 
 Chehov_toska = Book(
-    'Toska', 
-    'Chehov', 
-    550, 
-    '888-5-94708-243-8', 
+    'Toska',
+    'Chehov',
+    550,
+    '888-5-94708-243-8',
     False
     )
 
 Gogol_igroki = Book(
-    'Igroki', 
-    'Gogol', 
-    1050, 
-    '777-5-94708-243-8', 
+    'Igroki',
+    'Gogol',
+    1050,
+    '777-5-94708-243-8',
     False
     )
 
 Pushin_osen = Book(
-    'Osen', 
-    'Pushkin', 
-    2110, 
-    '111-5-94708-243-2', 
+    'Osen',
+    'Pushkin',
+    2110,
+    '111-5-94708-243-2',
     True
     )
 
@@ -74,49 +75,51 @@ for book in books_list:
 
 
 class SchoolBooks(Book):
-    
-    def __init__(
-            self, 
-            book_name, 
-            author, 
-            pages, 
-            ISBN, 
-            reserved, 
-            school_predmet, 
-            school_class, 
+
+    def __init__(self,
+            book_name,
+            author,
+            pages,
+            ISBN,
+            reserved,
+            school_predmet,
+            school_class,
             school_homework
             ):
         super().__init__(
-            book_name, 
-            author, 
-            pages, 
-            ISBN, 
+            book_name,
+            author,
+            pages,
+            ISBN,
             reserved
             )
         self.school_predmet = school_predmet
         self.school_class = school_class
         self.school_homework = school_homework
 
+
+# пишу список школьных книг
 school_book_first = SchoolBooks(
-    'Математика для школьников', 
-    'Иванова А.А.', 
-    301, 
-    '111-222-333', 
-    False, 
-    'Математика', 
-    '7 класс', 
+    'Математика для школьников',
+    'Иванова А.А.',
+    301,
+    '111-222-333',
+    False,
+    'Математика',
+    '7 класс',
     False
     )
 
 school_book_second = SchoolBooks(
-    'История для школьников', 
-    'Петров А.А.', 
-    101, 
-    '000-42-333', 
-    True, 
-    'История', 
-    '10 класс', 
-    True)
+    'История для школьников',
+    'Петров А.А.',
+    101,
+    '000-42-333',
+    True,
+    'История',
+    '10 класс',
+    True
+    )
 
 school_book_list = [school_book_first, school_book_second]
 
